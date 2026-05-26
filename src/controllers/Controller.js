38eae0f3eec61cd0c333b59daf40dde8f7848cab
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-05-26 19:34:19 -0300

    cria metodo no controller que atualiza registro

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,6 +8,26 @@ class Controller {
             const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
             return res.status(200).json(listaDeRegistro)
         } catch (error) {
+            return res.status(500).json({ message: 'Ocorreu um erro ao buscar os registros' });
+        }
+    }
+
+    async atualiza(req, res) {
+        const { id } = req.params;
+        const dadosAtualizados = req.body;
+
+        try {
+            // foiAtualizado esta retornando como undefined
+            const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+            
+            if(!foiAtualizado) {
+                return res.status(400).json({ message: 'Não foi possível atualizar o registro' })
+            }
+
+            return res.status(200).json({ message: 'Registro atualizado com sucesso'});
+            
+        } catch (error) {
+            return res.status(500).json({ message: `Ocorreu um erro ao atualizar o registro, ${error.message}` });
             
         }
     }

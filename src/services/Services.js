b6f55ee99d4b2da1caa9d15c749d0bdfeb9bcf56
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-05-26 19:36:36 -0300

    cria metodo no service para atualizar registo para comunicar com o controller

--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,19 @@ class Services {
     async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
     }
+
+    async atualizaRegistro(dadosAtualizado, id) {
+        const listaDeRegistroAtualizado = await dataSource[this.model].update(dadosAtualizado, {
+            where: {
+                id: id
+            }
+        })
+
+        if(listaDeRegistroAtualizado[0] === 0) {
+            return false;
+        }
+        return true;
+    }
 }
 
 module.exports = Services;

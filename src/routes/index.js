67888659f6498038716ee4338be954a86c72cf01
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-06-10 18:23:53 -0300

    adciona novas rotas da api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const pessoasRoute = require('./pessoasRoute.js');
+const categoriasRoute = require('./categoriasRoute.js');
+const cursosRoute = require('./cursosRoute.js');
 
 module.exports = app => {
     app.use(pessoasRoute);
+    app.use(categoriasRoute);
+    app.use(cursosRoute);
 }
 

e4e5e316bed7cb1c40b50029b1fd8fe9743cc34a
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-06-10 18:24:53 -0300

    adiciona novos controllers para comunicar com seus services

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -12,6 +12,39 @@ class Controller {
         }
     }
 
+    async pegaUmPorId(req, res) {
+        const { id } = req.params;
+        
+        try {
+            const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
+
+            if(!umRegistro) {
+                return res.status(404).json({message: 'Registro não encontrado'});
+            }
+
+            return res.status(200).json(umRegistro);
+        }catch (error) {
+            return res.status(500).json({ message: `Ocorreu um erro ao buscar o registro, ${error.message}` });
+        }
+    }
+
+    async criaNovo(req, res) {
+        const dadosParaCriacao = req.body;
+
+        try {
+             const novoRegistro = await this.entidadeService.criaRegistro(dadosParaCriacao);
+            console.log(novoRegistro);
+
+            if(!novoRegistro) {
+                return res.status(400).json({ message: 'Não foi possivel criar o registro' });
+            }
+
+            return res.status(201).json(novoRegistro);
+        }catch (error) {
+            return res.status(500).json({ message: `Ocorreu um erro ao criar o registro, ${error.message}` });
+        }
+    }
+
     async atualiza(req, res) {
         const { id } = req.params;
         const dadosAtualizados = req.body;
@@ -29,6 +62,18 @@ class Controller {
         } catch (error) {
             return res.status(500).json({ message: `Ocorreu um erro ao atualizar o registro, ${error.message}` });
             
+        }
+    }
+
+    async exclui(req, res) {
+        const { id } = req.params;
+
+        try {
+            await this.entidadeService.excluiRegistro(Number(id));
+            return res.status(200).json({ message: 'Registro excluído com sucesso' });
+
+        }catch (error) {
+            return res.status(500).json({ message: `Ocorreu um erro ao excluir o registro, ${error.message}` });
         }
     }
 }
--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -7,6 +7,24 @@ class PessoaController extends Controller {
     constructor() {
         super(pessoaServices);
     }
+
+    async pegaMatriculas(req, res) {
+        const { estudanteId } = req.params;
+
+        try {
+            const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+            console.log(listaMatriculas);
+
+            if(!listaMatriculas) {
+                return res.status(404).json({ message: 'Nenhuma Matricula encontrada para este estudante'});
+            }
+
+            return res.status(200).json(listaMatriculas);
+
+        }catch (error) {
+            return res.status(500).json({ message: `Ocorreu um erro ao buscar as matrículas, ${error.message}` });
+        }
+    }
 }
 
 module.exports = PessoaController;

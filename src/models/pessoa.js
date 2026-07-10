26554c4ef292eebd15e948b6f7b6ac70659b6ba1
GUSTAVO ALVES <gustavo.alves@HISPAGNOL.CORP>
2026-07-10 10:38:01 -0300

    adciona scope para pegar todos os registros, ativos ou nao

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -1,4 +1,6 @@
 'use strict';
+const isCpfValido = require('../utils/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -15,9 +17,33 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: 'O campo nome deve ter no minimo 3 caracteres e no maximo 30 caracteres'
+        }
+      }
+    },
+    email: { 
+      type: DataTypes.STRING, 
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'formato do email inválido'
+        }
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfIsValid: (cpf) => {
+          if(!isCpfValido(cpf)) throw new Error('Numero de CPF inválido');
+        }
+      }
+    }, 
+
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
@@ -29,7 +55,13 @@ module.exports = (sequelize, DataTypes) => {
       where: {
         ativo: true
       }
+    },
+    scopes: {
+      todosOsRegistros: {
+        where: {}
+      }
     }
+
   });
   return Pessoa;
 };
